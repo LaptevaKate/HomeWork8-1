13b4c8d3efad54409c8c1eac532e81228fc8183f
LaptevaKate <ekaterina213@yandex.ru>
2022-03-03 14:56:25 +0300

    add first part

---
+++ HomeWork8-1/FirstTaskViewController.swift
@@ -0,0 +1,44 @@
+//
+//  FirstTaskViewController.swift
+//  HomeWork8-1
+//
+//  Created by Екатерина Лаптева on 3.03.22.
+//
+
+import UIKit
+
+class FirstTaskViewController: UIViewController {
+
+    var views:[UIView] = []
+    
+    @IBAction func close(_ sender: Any) {
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
+    
+    @IBAction func tap(_ sender: UITapGestureRecognizer) {
+        let location = sender.location(in: view)
+       
+        let movingView = UIView()
+        movingView.backgroundColor = .purple
+        movingView.frame = CGRect(x: location.x - 30, y: location.y - 30, width: 60, height: 60)
+        movingView.layer.cornerRadius = 30
+        
+        for element in 0..<views.count {
+            if views[element].frame.contains(location){
+                views[element].removeFromSuperview()
+                views.remove(at: element)
+                return
+            }
+        }
+        views.append(movingView)
+        view.addSubview(movingView)
+    }
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
---
+++ HomeWork8-1/SecondTaskViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondTaskViewController.swift
+//  HomeWork8-1
+//
+//  Created by Екатерина Лаптева on 3.03.22.
+//
+
+import UIKit
+
+class SecondTaskViewController: UIViewController {
+
+    
+    @IBAction func close(_ sender: Any) {
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
